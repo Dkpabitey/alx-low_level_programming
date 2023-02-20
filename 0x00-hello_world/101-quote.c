e4f0dc201d992e5d5596364e1c13633a45ab77e7
Kpabitey Celestine Dedo <kpabiteycelestinededo@gmail.com>
2023-02-20 14:30:08 -0800

    he

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main() 		
 {
-  const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  const char* message = "and that piece of art is useful\n" - "Dora Korpar, 2015-10-19\n";
   write(2, message, 42);
-  return 1;
+  return (1);
 }
